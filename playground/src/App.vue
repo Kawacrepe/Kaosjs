9bb8c186307b2842fff59dc61951158f1c907c1e
Vincent Dhennin <vincent.dhennin@viacesi.fr>
2020-03-05 21:04:38 +0100

    add playgroundComponent + popOver example

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,15 +1,28 @@
 <template>
     <div id="#app" class="w-full">
         <div class="max-w-4xl p-4 mx-auto my-64" style="height: 400px;">
-            Tailwind ok
+            <div class="p-6 bg-gray-800 rounded shadow-xl">
+                <h1>
+                    Playground Components
+                </h1>
+                <div class="flex">
+                    <playground-component text="On click" class="ml-2" />
+                    <playground-component text="On hover" class="ml-2" trigger="hover" />
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from 'vue';
+import playgroundComponent from './components/playgroundComponent.vue';
+
 export default Vue.extend({
     name: 'App',
+    components: {
+        playgroundComponent,
+    }
 })
 </script>
 
---
+++ playground/src/components/playgroundComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <toggle v-slot="{ toggled, toggle, on, off }" :off-on-blur="true">
+    <!-- Trigger -->
+    <button
+      class="px-4 py-2 bg-gray-700 rounded shadow-lg focus:bg-gray-800"
+      ref="trigger"
+      @mouseenter="hoverMode ? on() : undefined"
+      @mouseleave="hoverMode ? off() : undefined"
+      @click="clickMode ? toggle() : undefined"
+      v-text="text"
+    />
+
+    <!-- Content -->
+    <div class="absolute p-4 mt-2 bg-gray-600 rounded shadow-xl" v-if="toggled">
+      This is the content. <br />
+      It's not necessarily a list, you can add anything there.
+    </div>
+  </toggle>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    name: 'playgroundComponent',
+    props: {
+        trigger: {
+            type: String,
+            default: 'click',
+            validator: (value: string) => ['hover', 'click'].includes(value),
+        },
+        text: {
+            type: String,
+            default: 'Popover',
+        },
+    },
+    computed: {
+        hoverMode() {
+            return 'hover' === this.trigger;
+        },
+        clickMode() {
+            return 'click' === this.trigger;
+        },
+    },
+})
+</script>
